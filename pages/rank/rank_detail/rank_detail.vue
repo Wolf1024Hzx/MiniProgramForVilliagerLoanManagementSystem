a36a74ba7dbb641902d4abbe565e4da12e5d6829
Wolf1024Hzx <hezhaoxi1024@outlook.com>
2022-08-27 19:51:06 +0800

    feat: 新增排行榜更多页面

---
+++ pages/rank/rank_detail/rank_detail.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<cu-custom :isBack="true">
+			<block slot="content">各行政区占比</block>
+		</cu-custom>
+		<view class="bg"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.bg {
+		width: 100%;
+		height: 187rpx;
+		background: linear-gradient(0deg, #4e8bff 5%, #52b0ff 94%);
+		position: absolute;
+		z-index: 0;
+		top: 0;
+	}
+</style>
