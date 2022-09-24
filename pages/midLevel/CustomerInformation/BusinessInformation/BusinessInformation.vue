c5cd57f2c6339ac5588984eb44b4833788438cd3
Wolf1024Hzx <hezhaoxi1024@outlook.com>
2022-09-24 21:57:59 +0800

    fixed: 新增中层业务信息页

---
+++ pages/midLevel/CustomerInformation/BusinessInformation/BusinessInformation.vue
@@ -0,0 +1,44 @@
+<template>
+  <view style="margin-top: 26rpx;" class="scroll_background">
+    <scroll-view scroll-x="true" class="scroll-view_H">
+      <view class="scroll_item">基本信息1</view>
+      <view class="scroll_item">基本信息2</view>
+      <view class="scroll_item">业务信息</view>
+      <view class="scroll_item">额度依据</view>
+      <view class="scroll_item">关联业务</view>
+    </scroll-view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+	.scroll_background {
+		background: #ffffff;
+		
+		.scroll-view_H {
+				white-space: nowrap;
+				width: 100%;
+				
+				.scroll_item {
+					display: inline-block;
+					font-size: 28rpx;
+					font-family: Source Han Sans CN, Source Han Sans CN-Regular;
+					font-weight: 400;
+					text-align: center;
+					color: #2d2f31;
+					padding: 20rpx;
+				}
+		}
+	}
+</style>
---
+++ pages/midLevel/CustomerInformation/CustomerInformationSearch/CustomerInformationSearch.vue
@@ -0,0 +1,38 @@
+<template>
+  <view>
+    <!-- <uni-search-bar placeholder="请输入您的证件号码" bg-color="#FFFFFF" cancel-button="none" clear-button="auto" /> -->
+    <view class="search">
+      <input style="width: 100%;" type="text" placeholder="请输入您的证件号码">
+      <img class="search-icon" src="@/static/midLevel/search.png">
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less">
+	.search {
+		width: 100%;
+		height: 96rpx;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 0 32rpx;
+		background-color: #fff;
+		margin-bottom: 30rpx;
+		margin-top: 42rpx;
+
+		.search-icon {
+			height: 37rpx;
+			width: 36rpx;
+		}
+	}
+</style>
