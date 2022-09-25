e529ea635dbd8366bcb6d0523ee462a5320be7e5
ITBock <2539214147@qq.com>
2022-09-25 14:29:01 +0800

    feat:担保圈

--- pages/midLevel/guaranteeCircle/guaranteeCircle.vue
+++ pages/midLevel/guaranteeCircle/guaranteeCircle.vue
@@ -9,6 +9,28 @@
 					<view v-for="(item, index) in classifyChoses" @click="bindClassify(index)" :class="classifyStatus === index ? 'classify-chose-active' : 'classify-chose'">{{item}}</view>
 				</scroll-view>
 			</view>
+			<view class="info">
+				<view v-for="(item, index) in guarateeInfos" class="card">
+					<view class="card-head">{{item.type}}</view>
+					<view class="card-body">
+						<view class="card-body-row">
+							<view class="key">组数</view>
+							<view class="value">{{item.groupNum}}</view>
+						</view>
+						<view class="card-body-row">
+							<view class="key">人数</view>
+							<view class="value">{{item.memberNum}}</view>
+						</view>
+						<view class="card-body-row">
+							<view class="key">金额</view>
+							<view class="value">{{item.price}}元</view>
+						</view>
+					</view>
+					<view class="card-hail">
+						<view class="btn">去查看</view>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -20,23 +42,29 @@ export default {
 		return {
 			classifyStatus: 3,
 			classifyChoses: ['业务信息','额度依据','关联业务','对外担保','担保圈'],
-			userInfos: [{
-				username: '张三',
-				serviceType: '/',
-				account: 300000,
-				balance: 1000000,
-				loanBegin: '2022年11月21日',
-				loanEnd: '2023年11月21日',
-				loan: 150000
+			guarateeInfos: [{
+				type: 'AB互保',
+				groupNum: 3,
+				memberNum: 15,
+				price: 150000
 			},
 			{
-				username: '张三',
-				serviceType: '/',
-				account: 300000,
-				balance: 1000000,
-				loanBegin: '2022年11月21日',
-				loanEnd: '2023年11月21日',
-				loan: 150000
+				type: 'ABC担保圈',
+				groupNum: 3,
+				memberNum: 15,
+				price: 150000
+			},
+			{
+				type: 'ABCD担保圈',
+				groupNum: 3,
+				memberNum: 15,
+				price: 150000
+			},
+			{
+				type: '5P及以上担保圈',
+				groupNum: 3,
+				memberNum: 15,
+				price: 150000
 			}]
 		}
 	},
@@ -84,5 +112,67 @@ export default {
 	color: #5484FD;
 	border-bottom: 3rpx solid #5484FD;
 	margin-right: 48rpx;
+}
+.key {
+	opacity: 0.8;
+	font-size: 28rpx;
+	font-weight: 400;
+	color: #2D2F31;
+}
+.value {
+	font-size: 28rpx;
+	font-weight: 700;
+	color: #2D2F31;
+}
+.info {
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	padding: 0 30rpx;
+	flex-wrap: wrap;
+}
+.card {
+	margin-top: 32rpx;
+	width: 330rpx;
+	height: 382rpx;
+	background: #ffffff;
+	border-radius: 10rpx;
+}
+.card-head {
+	height: 75rpx;
+	width: 100%;
+	background: #5484fd;
+	border-radius: 10rpx 10rpx 0rpx 0rpx;
+	color: #ffffff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.card-body {
+	height: 180rpx;
+	margin: 24rpx 20rpx;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+}
+.card-body-row {
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+}
+.card-hail {
+	display: flex;
+	justify-content: center;
+}
+.btn {
+	width: 200rpx;
+	height: 55rpx;
+	border: 1rpx solid #5484fd;
+	border-radius: 29rpx;
+	background: #ffffff;
+	color: #4e8bff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
